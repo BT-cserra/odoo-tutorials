2b61cacab13d6e3773c744ae7f097045a16e65cc
Carlos Serra-Toro <carlos.serra@braintec.com>
2023-05-19 11:02:32 +0200

    6. Adding a todo

--- owl_playground/static/src/todo_list/todo_list.js
+++ owl_playground/static/src/todo_list/todo_list.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 
 import { Todo } from "../todo/todo"
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 
 export class TodoList extends Component {
@@ -10,10 +10,21 @@ export class TodoList extends Component {
     static components = { Todo };
 
     setup() {
-        this.todoList = [
+        this.todoList = useState([
             {id: 1, description: "one", done: false},
             {id: 2, description: "two", done: true},
             {id: 3, description: "three", done: true},
-        ];
+        ]);
+        this.nextId = this.todoList.length + 1;
     }
+
+    addTodo(ev) {
+        if (ev.keyCode === 13 && ev.target.value.length > 0) {
+            this.todoList.push(
+                {id: this.nextId++, description: ev.target.value, done: false}
+            )
+            ev.target.value = "";
+        }
+    }
+
 }
