b34cc18d166b1c69bdaff821bb2c2aad55d1752d
Carlos Serra-Toro <carlos.serra@braintec.com>
2023-05-24 15:24:22 +0200

    11. Extensive props validation

--- owl_playground/static/src/card/card.js
+++ owl_playground/static/src/card/card.js
@@ -4,4 +4,14 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "owl_playground.Card";
+
+    static props = {
+        slots: {
+            type: Object,
+            shape: {
+                default: Object,
+                title: {type: Object, optional: true},
+            },
+        },
+    };
 }
